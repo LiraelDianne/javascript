4233641228cd2b50b2cd1efd50f72c312e4e977a
Crystal Stellwagen <liraeldianne@gmail.com>
2015-05-25 16:46:05 -0700

    homework 7

--- F1-Javascript/Donut_Shop_Interactive.js
+++ F1-Javascript/Donut_Shop_Interactive.js
@@ -1,8 +1,9 @@
 function DonutShop(name, minc, maxc, donuts, hours) {
-
+  this.minCust = minc;
+  this.maxCust = maxc;
   this.name = name;
   this.customers = function() {
-    var avgCust = Math.floor(Math.random() * (maxc - minc + 1)) + minc;
+    var avgCust = Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
     return avgCust;
   };
 
@@ -10,13 +11,14 @@ function DonutShop(name, minc, maxc, donuts, hours) {
   this.hoursOpen = hours;
 
   //I put this in a variable so I could retrieve the same number when I calculate the donuts per day
-  this.donutsPerHour = Math.ceil(this.customers() * this.donutsPerCustomer);
+  this.donutsPerHour = 0;
   this.getDonutsPerHour = function() {
-    return this.donutsPerHour;
+    this.donutsPerHour = Math.ceil(this.customers() * this.donutsPerCustomer);
   };
+  this.getDonutsPerHour();
 
   this.getDonutsPerDay = function() {
-    return Math.ceil(this.getDonutsPerHour() * hours)
+    return Math.ceil(this.donutsPerHour * hours)
   };
 
   this.message = this.name + " should bake " + this.donutsPerHour + " donuts per hour and " + this.getDonutsPerDay() + " donuts per day.";
@@ -36,10 +38,29 @@ function DonutMaster() {
       }
     }
   }
-
-  this.generateReport = function(shopList) {
+  var that = this;
+  this.generateReport = function() {
     for (var i = 0; i < this.shopList.length; i++) {
-      $('tbody').append("<tr><th>"+this.shopList[i].name+"</th><td>"+this.shopList[i].donutsPerHour+"</td><td>"+this.shopList[i].getDonutsPerDay()+"</td></tr>");
+      $('tbody').append("<tr id='"+i+"'><th>"+this.shopList[i].name
+        +"</th><td class='edit'>Edit</td><td class='perHour'>"
+        +this.shopList[i].donutsPerHour+"</td><td class='perDay'>"
+        +this.shopList[i].getDonutsPerDay()+"</td></tr>");
+      var inputMinC = "<input type='text' name='minc' placeholder='minCustomers' />";
+      var inputMaxC = "<input type='text' name='maxc' placeholder='maxCustomers' />";
+      var inputAvgDonuts = "<input type='text' name='donutsPerC' placeholder='donutsPerCustomer' />";
+      var inputHours = "<input type='text' name='hours' placeholder='hours' />";
+      $('#'+i).append("<td class='form'><form>"+inputMinC+inputMaxC+inputAvgDonuts+inputHours+"<button id='btn"+i+"'>Update</button></form></td>");
+      $('#btn'+i).on("click", function(e) {
+        e.preventDefault();
+        var j = ($(this).attr("id").slice(3));
+        that.shopList[j].minCust = $(this).siblings("[name='minc']").val();
+        that.shopList[j].maxCust = $(this).siblings("[name='maxc']").val();
+        that.shopList[j].hoursOpen = $(that).siblings("[name='hours']").val();
+        that.shopList[j].donutsPerCustomer = $(this).siblings("[name='donutsPerC']").val();
+        that.shopList[j].getDonutsPerHour();
+        $('#'+j+' > .perHour').text(that.shopList[j].donutsPerHour);
+        $('#'+j+' > .perDay').text(that.shopList[j].getDonutsPerDay());
+      });
     }
   }
 }
@@ -51,11 +72,14 @@ master.addShop("slu", "South Lake Union", 9, 23, 6.33, 12);
 master.addShop("wedgewood", "Wedgewood", 2, 28, 1.25, 12);
 master.addShop("ballard", "Ballard", 8, 58, 3.75, 12);
 master.generateReport();
-$('#perHour').hide();
-$('#perDay').hide();
+$('#perHour', '#perDay').addClass("invisible");
 $('td').hide();
 $("th").on("click", function(e) {
-  $('#perHour').fadeIn('slow');
-  $('#perDay').fadeIn('slow');
-  $(this).siblings('td').fadeToggle();
+  $('#perHour', '#perDay').removeClass("invisible");
+  $(this).siblings('.edit').fadeToggle();
+  $(this).siblings('.perDay').fadeToggle();
+  $(this).siblings('.perHour').fadeToggle();
+});
+$('.edit').on("click", function(e) {
+  $(this).siblings('.form').fadeToggle();
 });
